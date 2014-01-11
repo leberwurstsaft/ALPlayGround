56fb521d4c4ab51aba546366f04508bc9ed91031
Pit Garbe <leberwurstsaft@me.com>
2014-01-11 20:22:53 +0100

    Landscape and Portrait are working!

--- ALPlayGround/ALPlayGround/ALPViewController.m
+++ ALPlayGround/ALPlayGround/ALPViewController.m
@@ -9,7 +9,13 @@
 #import "ALPViewController.h"
 
 @interface ALPViewController ()
+
 @property (strong, nonatomic) IBOutletCollection(NSLayoutConstraint) NSArray *layoutConstraintsPortrait;
+@property (strong, nonatomic) NSMutableArray *layoutConstraintsLandscape;
+
+
+@property (strong, nonatomic) IBOutlet UIView *green;
+@property (strong, nonatomic) IBOutlet UIView *orange;
 
 @end
 
@@ -19,7 +25,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"constraints: %@", self.layoutConstraintsPortrait);
+}
+
+- (void)updateViewConstraints {
+    [super updateViewConstraints];
+    
+    NSDictionary *views = NSDictionaryOfVariableBindings(_green, _orange);
+    if (!self.layoutConstraintsLandscape) {
+        self.layoutConstraintsLandscape = [NSMutableArray arrayWithArray:
+                                           [NSLayoutConstraint constraintsWithVisualFormat:@"H:[_green]-16-[_orange]"
+                                                                                   options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                                   metrics:nil
+                                                                                     views:views]];
+        [self.layoutConstraintsLandscape addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[_orange]"
+                                                                                            options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                                            metrics:nil
+                                                                                              views:views]];
+        [self.layoutConstraintsLandscape addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_green]-20-|"
+                                                                                                     options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                                                     metrics:nil
+                                                                                                       views:views]];
+    }
+
+    [self.view removeConstraints:self.layoutConstraintsLandscape];
+    [self.view removeConstraints:self.layoutConstraintsPortrait];
+    [self.view addConstraints:(UIInterfaceOrientationIsPortrait(self.interfaceOrientation)) ? self.layoutConstraintsPortrait : self.layoutConstraintsLandscape];
 }
 
 - (void)didReceiveMemoryWarning
